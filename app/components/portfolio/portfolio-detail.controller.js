d58b8b3dde72ad6f4f9c93859af9733aaf5b2a04
njausteve <njaustevedomino@gmail.com>
2017-10-28 18:01:25 +0300

    added changes to portfolio details

---
+++ app/components/portfolio/portfolio-detail.controller.js
@@ -0,0 +1,6 @@
+/**
+ * (C) njausteve, 2017
+ *  Resume
+ * by Stephen njau(njaustevedomino@gmail.com)
+ */
+!function(){"use strict";function t(t){var e=this;e.details=t.projectDetails}t.$inject=["$stateParams"],angular.module("njausteve").controller("portifolioDetailsController",t)}();
